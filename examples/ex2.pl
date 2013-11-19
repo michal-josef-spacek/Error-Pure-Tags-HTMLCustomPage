9a0a4f2be9dff0f00183901f8379fc5299717a99
Michal Špaček <tupinek@gmail.com>
2013-11-19 14:14:15 +0100

    Add examples/ directory.

---
+++ examples/ex2.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Modules.
+use Error::Pure::Tags::HTMLCustomPage qw(err);
+
+# Error.
+err "1";
+
+# Output like:
+# Bad 'Tags' object at %s/HTMLCustomPage.pm line 56.
