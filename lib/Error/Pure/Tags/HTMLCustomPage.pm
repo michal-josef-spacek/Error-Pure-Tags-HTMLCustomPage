e4c558a157e6b22eab55634dd2d930017415b2e5
Michal Špaček <tupinek@gmail.com>
2013-11-19 14:13:58 +0100

    Add third example in doc. Optimalize second example in doc.

--- lib/Error/Pure/Tags/HTMLCustomPage.pm
+++ lib/Error/Pure/Tags/HTMLCustomPage.pm
@@ -185,10 +185,42 @@ Error::Pure::Tags::HTMLCustomPage - Error::Pure module with output as Tags custo
  # Cache-Control: no-cache
  # Date: Tue, 19 Nov 2013 13:05:29 GMT
  # Content-Type: application/xhtml+xml
- # 
- # <?xml version="1.1" encoding="UTF-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- # "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><head><title>Error</title></head><body><div>Error
- # on page</div></body></html>
+ #
+ # <?xml version="1.1" encoding="UTF-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><head><title>Error</title></head><body><div>Error on page</div></body></html>
+
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Error::Pure::Tags::HTMLCustomPage qw(err);
+ use Tags::Output::Indent;
+
+ # Tags object.
+ $Error::Pure::Tags::HTMLCustomPage::TAGS = Tags::Output::Indent->new;
+
+ # Error.
+ err "1";
+
+ # Output like:
+ # Cache-Control: no-cache
+ # Date: Tue, 19 Nov 2013 13:10:44 GMT
+ # Content-Type: application/xhtml+xml
+ #
+ # <?xml version="1.1" encoding="UTF-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html>
+ #   <head>
+ #     <title>
+ #       Error
+ #     </title>
+ #   </head>
+ #   <body>
+ #     <div>
+ #       Error on page
+ #     </div>
+ #   </body>
+ # </html>
 
 =head1 DEPENDENCIES
 
