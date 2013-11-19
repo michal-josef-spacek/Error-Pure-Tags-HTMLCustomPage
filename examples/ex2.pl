8c55413f741055c6b05fcf06276aff4fb5c7ff3a
Michal Špaček <tupinek@gmail.com>
2013-11-19 15:19:24 +0100

    Move Error::Pure::Output::Tags::HTMLCustomPage to another distribution.

--- examples/ex2.pl
+++ examples/ex2.pl
@@ -6,9 +6,17 @@ use warnings;
 
 # Modules.
 use Error::Pure::Tags::HTMLCustomPage qw(err);
+use Tags::Output::Raw;
+
+# Tags object.
+$Error::Pure::Tags::HTMLCustomPage::TAGS = Tags::Output::Raw->new;
 
 # Error.
 err "1";
 
 # Output like:
-# Bad 'Tags' object at %s/HTMLCustomPage.pm line 56.
+# Cache-Control: no-cache
+# Date: Tue, 19 Nov 2013 13:05:29 GMT
+# Content-Type: application/xhtml+xml
+#
+# <?xml version="1.1" encoding="UTF-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><head><title>Error</title></head><body><div>Error on page</div></body></html>
